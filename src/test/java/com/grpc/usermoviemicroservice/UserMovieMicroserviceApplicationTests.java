4de6cb224e9a3e0017ef0b92a26c0eee7fb4744d
kuhuisien <>
2022-04-02 17:49:32 +0800

    project setup, add dependencies for modules

---
+++ src/test/java/com/grpc/usermoviemicroservice/UserMovieMicroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.grpc.usermoviemicroservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserMovieMicroserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
