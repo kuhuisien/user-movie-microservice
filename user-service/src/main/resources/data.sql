8652f0699ae0cbf553dfa24df72e3a33a50d3c56
kuhuisien <>
2022-04-06 22:17:30 +0800

    refine user data.sql to enforce NOT NULL id

--- user-service/src/main/resources/data.sql
+++ user-service/src/main/resources/data.sql
@@ -1,7 +1,7 @@
 DROP TABLE IF EXISTS user;
 
 CREATE TABLE user(
-login_id VARCHAR(50),
+login_id VARCHAR(50) NOT NULL,
 name VARCHAR(50) NOT NULL,
 genre VARCHAR(50) NOT NULL
 ) AS SELECT * FROM CSVREAD('classpath:user.csv');
